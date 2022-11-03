615ec10ef16ed553512046762c0fbce4a5e421c3
trinhquanjvb <trinhquan.jvb@gmail.com>
2022-11-03 08:42:41 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express') // nạp thư viện express
+const app = express() // gán
+const port = 3000 // cổng
+
+// get là router
+app.get('/Home', (req, res) => {
+   res.send('Hello World!')
+})
+
+app.listen(port, () => {
+   console.log(`${port}`)
+})
